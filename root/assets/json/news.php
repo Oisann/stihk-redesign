95f4b4cb05c6ce91fc48d6afec47a1183557727b
Jonas <jonas@oisann.net>
2014-06-10 17:25:09 +0200

    Small config fix

--- root/assets/json/news.php
+++ root/assets/json/news.php
@@ -1,12 +1,12 @@
 <?php
-	include('../config.php');
+	include('../../config.php');
 	header('Cache-Control: no-cache, must-revalidate');
 	header('Expires: ' . date("D, d M Y G:i:s T", strtotime("+1 week")) );
 	header('Content-type: application/json');
 
 	$mysql_connection = mysql_connect($mysql_host,$mysql_user,$mysql_password);
 	if (!$mysql_connection) die('{ "error":"mysql connection failed" }');
-	if (!mysql_select_db($db, $mysql_connection)) die('{ "error":"mysql database not found" }');
+	if (!mysql_select_db($mysql_database, $mysql_connection)) die('{ "error":"mysql database not found" }');
 	$mysql_selector = "SELECT * FROM stihk_nyheter WHERE (nyhet_skjult = 0) ORDER BY nyhet_id DESC LIMIT 0, 10;";
 	
 	$mysql_query = mysql_query($mysql_selector, $mysql_connection);
