9bc855b26b64ce822fa55cf365f0710327259df9
Jonas <jonas@oisann.net>
2014-07-03 15:14:42 +0200

    function fails... pls help

--- root/classes/funksjoner.class.php
+++ root/classes/funksjoner.class.php
@@ -3,8 +3,8 @@
 	class funksjoner {
 
 		public function sesong() {
-			if(date("n") <= 5) return "" . date("Y") - 1 . "/" . date("Y");
-			return "" . date("Y") . "/" . date("Y") + 1;
+			if(date("n") <= 5) return "SESONG" . date("Y") - 1 . "/" . date("Y");
+			return "SESONG" . date("Y") . "/" . date("Y") + 1;
 		}
 
 		public function neste_sesong() {
