7f2cde0009ded0666af414c4b68d06fa3de81862
Jonas <jonas@oisann.net>
2014-07-03 15:09:28 +0200

    Missing semi-colon

--- root/index.php
+++ root/index.php
@@ -11,8 +11,8 @@
 	if(!file_exists("./parts/pages/" . $page . ".php")) $page = "404";
 	$page_include = "./parts/pages/" . $page . ".php";
 
-	if($page == "404") $title = "Siden kan ikke vises - Error 404"
-
+	if($page == "404") $title = "Siden kan ikke vises - Error 404";
+	
 	include('./parts/header.php');
 	include($page_include);
 	include('./parts/footer.php');
