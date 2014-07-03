2e6b7a0a23093481a656419f0bb3f57ae63abf51
Jonas <jonas@oisann.net>
2014-07-03 15:47:31 +0200

    This should work

--- root/classes/funksjoner.class.php
+++ root/classes/funksjoner.class.php
@@ -3,13 +3,13 @@
 	class funksjoner {
 
 		public function sesong() {
-			if(date("n") <= 5) return "FØR JUNI"; //date("Y") - 1 . "/" . date("Y");
-			return "ETTTER JUNI"; //date("Y") . "/" . date("Y") + 1;
+			if(date("n") <= 5) return (date("Y") - 1) . "/" . date("Y");
+			return date("Y") . "/" . (date("Y") + 1);
 		}
 
 		public function neste_sesong() {
-			if(date("n") <= 5) return "" . date("Y") . "/" . date("Y") + 1;
-			return "" . date("Y") + 1 . "/" . date("Y") + 2;
+			if(date("n") <= 5) return date("Y") . "/" . (date("Y") + 1);
+			return (date("Y") + 1) . "/" . (date("Y") + 2);
 		}
 
 	}
