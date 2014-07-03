b00821133174a013d065c25d1e90170fdb8dfda6
Jonas <jonas@oisann.net>
2014-07-03 15:13:11 +0200

    Revert "Test fix"
    This reverts commit 9e381b7c4b9d6d26275510452185ad0bd564d2ed.

--- root/index.php
+++ root/index.php
@@ -2,7 +2,7 @@
 	session_start();
 	$classes = glob('./classes/*.class.php');
 	foreach($classes as $class) {
-		include('./classes/' . $class);
+		include($class);
 	}
 	$funksjoner = new funksjoner();
 	$page = $_GET['page'];
@@ -12,7 +12,7 @@
 	$page_include = "./parts/pages/" . $page . ".php";
 
 	if($page == "404") $title = "Siden kan ikke vises - Error 404";
-
+	
 	include('./parts/header.php');
 	include($page_include);
 	include('./parts/footer.php');
