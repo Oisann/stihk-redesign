e687cc35178629382ba971aa80fd786851f98a49
Jonas <jonas@oisann.net>
2014-08-01 19:14:41 +0200

    nyheter test

--- root/parts/pages/nyheter.php
+++ root/parts/pages/nyheter.php
@@ -1,3 +1,18 @@
 		<div class="news" style="padding-left: 10px; margin-right: -10px">
-			<h1>Nyheter.... <?php echo $id; ?></h1>
+<?php
+
+	$mysql_connection = mysql_connect($mysql_host,$mysql_user,$mysql_password);
+	if (!$mysql_connection) die('{ "error":"mysql connection failed" }');
+	if (!mysql_select_db($mysql_database, $mysql_connection)) die('{ "error":"mysql database not found" }');
+	$mysql_selector = "SELECT * FROM nyheter WHERE (id = " . $id . ") ORDER BY endret DESC LIMIT 0, 15;";
+
+	$mysql_query = mysql_query($mysql_selector, $mysql_connection);
+	if(!$mysql_query) die('{ "error":"' . mysql_error() . '" }');
+	
+	while ($row = mysql_fetch_array($mysql_query)) {
+		echo "<h1>" . $row['overskrift'] . "</h1>";
+		echo $row['text'];
+	}
+	mysql_close($mysql_connection);
+?>
 		</div>
