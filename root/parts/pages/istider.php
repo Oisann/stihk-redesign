34cd68468638cdb59490820de1eb6cb07bf692d7
Jonas <jonas@oisann.net>
2014-08-01 16:51:56 +0200

    hmm

--- root/parts/pages/istider.php
+++ root/parts/pages/istider.php
@@ -1,4 +1,4 @@
-		<div class="news" style="padding-left: 10px">
+		<div class="news" style="padding-left: 10px; margin-right: -10px">
 			<h1>Istider <?php echo $funksjoner->sesong(); ?></h1>
 			
 		</div>
