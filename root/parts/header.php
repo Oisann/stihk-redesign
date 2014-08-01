292805e4d3cb1299afa824bfe4ef0f6cd4b93f16
Jonas <jonas@oisann.net>
2014-08-01 20:02:52 +0200

    Welcome fix

--- root/parts/header.php
+++ root/parts/header.php
@@ -123,6 +123,6 @@
 				</table>
 			</div>
 			<div class="clock"></div>
-			<div class="newsfeed"><strong>Velkommen til STIHK.no</strong></div>
+			<div class="newsfeed"><strong>Velkommen til STIHK.no</strong> - </div>
 		</div>
 		<div class="shadow"></div>
