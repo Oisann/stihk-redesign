d3db933973f020239ef4fa4bb49e6d0fadcbf318
Jonas <jonas@oisann.net>
2014-08-01 19:19:59 +0200

    Test v3

--- root/parts/pages/nyheter.php
+++ root/parts/pages/nyheter.php
@@ -8,12 +8,14 @@
 
 	$mysql_query = mysql_query($mysql_selector, $mysql_connection);
 	if(!$mysql_query) die('{ "error":"' . mysql_error() . '" }');
-	
-	if(empty($mysql_query)) die('Nyhetsartikkelen ble ikke funnet... Error: 11');
 
 	while ($row = mysql_fetch_array($mysql_query)) {
+		if(empty($row)) {
+			echo "Fant ingen nyhet med id: " . $id;
+			return;
+		}
 		echo "<h1>" . $row['overskrift'] . "</h1>";
-		echo $row['tekst'];
+		echo utf8_decode($row['tekst']);
 	}
 	mysql_close($mysql_connection);
 ?>
