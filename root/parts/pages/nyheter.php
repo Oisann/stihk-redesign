e5fa15cfa9b1aee61c6d145cb8d5265cef4337bc
Jonas <jonas@oisann.net>
2014-08-01 19:25:24 +0200

    Test v6

--- root/parts/pages/nyheter.php
+++ root/parts/pages/nyheter.php
@@ -8,14 +8,13 @@
 
 	$mysql_query = mysql_query($mysql_selector, $mysql_connection);
 	if(!$mysql_query) die('{ "error":"' . mysql_error() . '" }');
-
+	if(mysql_fetch_array($mysql_query) == null) {
+		echo "			<h1>Error 11</h1>\n";
+		echo "			Fant ingen nyhet med id: " . $id;
+	}
 	while ($row = mysql_fetch_array($mysql_query)) {
-		if(empty($row['overskrift'])) {
-			echo "Fant ingen nyhet med id: " . $id;
-			return;
-		}
-		echo "<h1>" . $row['overskrift'] . "</h1>";
-		echo htmlentities($row['tekst']);
+		echo "			<h1>" . $row['overskrift'] . "</h1>\n";
+		echo "			" . htmlentities($row['tekst']);
 	}
 	mysql_close($mysql_connection);
 ?>
