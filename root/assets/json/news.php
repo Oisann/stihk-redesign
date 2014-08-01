0f43f861ed69850f5be5dd843a0428083af547e0
Jonas <jonas@oisann.net>
2014-08-01 17:25:48 +0200

    removed hidden tag

--- root/assets/json/news.php
+++ root/assets/json/news.php
@@ -14,7 +14,7 @@
 
 	$articles = array();
 	while ($row = mysql_fetch_array($mysql_query)) {
-		$articles[intval($row['id'])] = array('date' => intval($row['laget']), 'changed' => intval($row['endret']), 'type' => intval($row['type']), 'headline' => htmlentities($row['overskrift']), 'text' => htmlentities($row['tekst']), 'hidden' => 0);
+		$articles[intval($row['id'])] = array('date' => intval($row['laget']), 'changed' => intval($row['endret']), 'type' => intval($row['type']), 'headline' => htmlentities($row['overskrift']), 'text' => htmlentities($row['tekst']));
 	}
 	mysql_close($mysql_connection);
 	echo html_entity_decode(json_encode($articles));
