9924bef82f2873fa5dc22738d116ba9f135c8a5b
Jonas <jonas@oisann.net>
2014-08-01 19:41:21 +0200

    Istider

--- root/parts/pages/istider.php
+++ root/parts/pages/istider.php
@@ -1,4 +1,15 @@
 		<div class="news" style="padding-left: 10px; margin-right: -10px">
 			<h1>Istider <?php echo $funksjoner->sesong(); ?></h1>
-			
+			<div class="settings">
+				<select id="ishall">
+					<option selected>Leangen Arena</option>
+					<option>Leangen Ungdomshall</option>
+					<option>Dalgård</option>
+				</select>
+				<select id="uke">
+					<option selected>1 - 01.01.14 - 07.01.14</option>
+					<option>2 - 08.01.14 - 14.01.14<</option>
+					<option>3 - 15.01.14 - 22.01.14<</option>
+				</select>
+			</div>
 		</div>
