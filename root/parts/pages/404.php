d205e226d3217267b6201dc7ac40e4ed09f190e0
Jonas <jonas@oisann.net>
2014-08-01 19:56:32 +0200

    Error

--- root/parts/pages/404.php
+++ root/parts/pages/404.php
@@ -4,7 +4,7 @@
 				<tr><td><span class="loading" title="Loading..."></span></td></tr>
 			</table>
 		</div>
-		<div class="news">
+		<div class="news" style="width: 33.33%">
 			<h1 class="center">ERROR 404</h1>
 			<table class="stihknews">
 				<tr><td><h3>Siden du leter etter, finnes ikke!</h3></td></tr>
