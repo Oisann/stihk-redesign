7a76c4a857daef87efbe8e1241828918a6b50c0c
Jonas <jonas@oisann.net>
2014-08-02 17:39:57 +0200

    utf8 pls

--- root/assets/json/istider.php
+++ root/assets/json/istider.php
@@ -7,14 +7,14 @@
 	$mysql_connection = mysql_connect($mysql_host,$mysql_user,$mysql_password);
 	if (!$mysql_connection) die('{ "error":"mysql connection failed" }');
 	if (!mysql_select_db($mysql_database, $mysql_connection)) die('{ "error":"mysql database not found" }');
-	$mysql_selector = "SELECT * FROM ishaller WHERE (aktiv = 1 AND sortering != 999) ORDER BY sortering DESC;";
+	$mysql_selector = "SELECT * FROM ishaller WHERE (aktiv = 1 AND sortering != 999) ORDER BY sortering ASC;";
 	
 	$mysql_query = mysql_query($mysql_selector, $mysql_connection);
 	if(!$mysql_query) die('{ "error":"' . mysql_error() . '" }');
 
 	$articles = array();
 	while ($row = mysql_fetch_array($mysql_query)) {
-		$articles[intval($row['sortering'])] = array('id' => intval($row['id']), 'code' => $row['kode'], 'short' => $row['kortnavn'], 'name' => $row['navn'], 'listname' => $row['listenavn']);
+		$articles[intval($row['sortering'])] = array('id' => intval($row['id']), 'code' => htmlentities($row['kode']), 'short' => htmlentities($row['kortnavn']), 'name' => htmlentities($row['navn']), 'listname' => htmlentities($row['listenavn']));
 	}
 	mysql_close($mysql_connection);
 	echo utf8_encode(html_entity_decode(json_encode($articles)));
