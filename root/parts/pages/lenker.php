323cfcee273d72d6901209cd61906f9fa7aecf87
Jonas <jonas@oisann.net>
2014-08-02 20:24:30 +0200

    Lenker

---
+++ root/parts/pages/lenker.php
@@ -0,0 +1,108 @@
+﻿		<div class="news" style="padding-left: 10px; margin-right: -10px">
+			<h2>STIHK - Kretsen p&aring; nett</h2>
+			<h3>Klubbenes nettsider</h3>
+			<table border="1" cellspacing="1" cellpadding="2" bgcolor="#cccccc" bordercolor="#666666">
+			<tbody>
+			<tr>
+			<td class="fet" height="18" bgcolor="#ffffff">Klubb</td>
+			<td class="fet" height="18">&nbsp;</td>
+			<td class="fet" height="18" bgcolor="#ffffff">WEB</td>
+			</tr>
+			<tr>
+			<td>&nbsp;</td>
+			<td>&nbsp;</td>
+			<td>&nbsp;</td>
+			</tr>
+			<tr>
+			<td class="fet" height="18" bgcolor="#ffffff">Astor</td>
+			<td class="fet" height="18">&nbsp;</td>
+			<td class="fet" height="18" bgcolor="#ffffff"><a href="http://www.astorhockey.com" target="_blank">www.astorhockey.com</a></td>
+			</tr>
+			<tr>
+			<td class="fet" height="18" bgcolor="#ffffff">Flat&aring;s</td>
+			<td class="fet" height="18">&nbsp;</td>
+			<td class="fet" height="18" bgcolor="#ffffff"><a href="http://www.n3sport.no/Organisation.asp?OrgElementId=61421" target="_blank">www.n3sport.no/Organisation.asp?OrgElementId=61421</a></td>
+			</tr>
+			<tr>
+			<td class="fet" height="18" bgcolor="#ffffff">Fosen Hockey</td>
+			<td class="fet" height="18">&nbsp;</td>
+			<td class="fet" height="18" bgcolor="#ffffff"><a href="http://www.fosenhockey.com" target="_blank">www.fosenhockey.com</a></td>
+			</tr>
+			<tr>
+			<td class="fet" height="18" bgcolor="#ffffff">Leangen</td>
+			<td class="fet" height="18">&nbsp;</td>
+			<td class="fet" height="18" bgcolor="#ffffff"><a href="http://LIHK.no/" target="_blank">LIHK.no</a></td>
+			</tr>
+			<tr>
+			<td class="fet" height="18" bgcolor="#ffffff">Leik</td>
+			<td class="fet" height="18">&nbsp;</td>
+			<td class="fet" height="18" bgcolor="#ffffff"><a href="http://LEIK.no/" target="_blank">http://www.leik.no/</a></td>
+			</tr>
+			<tr>
+			<td class="fet" height="18" bgcolor="#ffffff">RIHK</td>
+			<td class="fet" height="18">&nbsp;</td>
+			<td class="fet" height="18" bgcolor="#ffffff"><a href="http://yngres.rihk.no/" target="_blank">http://yngres.rihk.no/</a></td>
+			</tr>
+			<tr>
+			<td class="fet" height="18" bgcolor="#ffffff">Tiller</td>
+			<td class="fet" height="18">&nbsp;</td>
+			<td class="fet" height="18" bgcolor="#ffffff"><a href="http://www.tillerhockey.no/" target="_blank">www.tillerhockey.no/</a></td>
+			</tr>
+			</tbody>
+			</table>
+			<h3>Lag p&aring; nett</h3>
+			<table border="1" cellspacing="1" cellpadding="2" bgcolor="#cccccc" bordercolor="#666666">
+			<tbody>
+			<tr>
+			<td class="fet" height="18" bgcolor="#ffffff">Lag</td>
+			<td class="fet" height="18">&nbsp;</td>
+			<td class="fet" height="18" bgcolor="#ffffff">WEB</td>
+			</tr>
+			<tr>
+			<td>&nbsp;</td>
+			<td>&nbsp;</td>
+			<td>&nbsp;</td>
+			</tr>
+			<tr>
+			<td class="fet" height="18" bgcolor="#ffffff">Astor Dreamteam 4. Div</td>
+			<td class="fet" height="18">&nbsp;</td>
+			<td class="fet" height="18" bgcolor="#ffffff"><a href="http://www.bts.as/dreamteam/" target="_blank">www.bts.as/dreamteam/</a></td>
+			</tr>
+			<tr>
+			<td class="fet" height="18" bgcolor="#ffffff">Atlantic Icebreakers 3. og 4. Div</td>
+			<td class="fet" height="18">&nbsp;</td>
+			<td class="fet" height="18" bgcolor="#ffffff"><a href="http://www.icebreakers.no/" target="_blank">www.icebreakers.no</a></td>
+			</tr>
+			<tr>
+			<td class="fet" height="18" bgcolor="#ffffff">Bambi Flyers 3. og 4. Div</td>
+			<td class="fet" height="18">&nbsp;</td>
+			<td class="fet" height="18" bgcolor="#ffffff"><a href="http://www.bambiflyers.com/" target="_blank">www.bambiflyers.com</a></td>
+			</tr>
+			<tr>
+			<td class="fet" height="18" bgcolor="#ffffff">Hippos Hockey 4. Div</td>
+			<td class="fet" height="18">&nbsp;</td>
+			<td class="fet" height="18" bgcolor="#ffffff"><a href="http://www.hippos.no" target="_blank">www.hippos.no</a></td>
+			</tr>
+			<tr>
+			<td class="fet" height="18" bgcolor="#ffffff">NTNUI 3. Div</td>
+			<td class="fet" height="18">&nbsp;</td>
+			<td class="fet" height="18" bgcolor="#ffffff"><a href="http://www.ntnui.no/ishockey" target="_blank">www.ntnui.no/ishockey</a></td>
+			</tr>
+			<tr>
+			<td class="fet" height="18" bgcolor="#ffffff">STIHK Jenter</td>
+			<td class="fet" height="18">&nbsp;</td>
+			<td class="fet" height="18" bgcolor="#ffffff"><a href="http://www.jentehockey.no" target="_blank">www.jentehockey.no</a></td>
+			</tr>
+			<tr>
+			<td class="fet" height="18" bgcolor="#ffffff">Sverresborg 3. Div</td>
+			<td class="fet" height="18">&nbsp;</td>
+			<td class="fet" height="18" bgcolor="#ffffff"><a href="http://www.bandithockey.no">www.<span style="text-decoration: underline;"><span style="color: #0000ff;">bandithockey.no</span></span></a></td>
+			</tr>
+			<tr>
+			<td class="fet" height="18" bgcolor="#ffffff">Wing 3. Div</td>
+			<td class="fet" height="18">&nbsp;</td>
+			<td class="fet" height="18" bgcolor="#ffffff"><a href="http://www.winghockey.no/" target="_blank">www.winghockey.no</a></td>
+			</tr>
+			</tbody>
+			</table>
+		</div>
