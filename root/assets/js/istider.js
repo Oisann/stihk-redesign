44a711d7f05743a7b0503ea0b79d645219c318d3
Jonas <jonas@oisann.net>
2014-08-02 18:25:47 +0200

    Now it should work

--- root/assets/js/istider.js
+++ root/assets/js/istider.js
@@ -9,13 +9,9 @@ $.ajax({
 function updateIshaller(json) {
 	console.log(json);
 	$('select#ishall').html("<option data-id=\"NaN\" disabled>Velg en ishall</option>");
-	var max = 0;
 	for (property in json) {
-	   console.log(property);
-	}
-	for(var i=0; i<max; i++) {
 		var disabled = "";
-		if(!json[i].enabled) disabled = " disabled";
-		$('select#ishall').append("<option data-id=\"" + json[i].id + "\" " + disabled + ">" + json[i].name + "</option>");
+		if(!json[property].enabled) disabled = " disabled";
+		$('select#ishall').append("<option data-id=\"" + json[property].id + "\" " + disabled + ">" + json[property].name + "</option>");
 	}
 }
