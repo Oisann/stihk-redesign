6e89818e81d1291cd8d5e94c949a1d18c159f305
Jonas <jonas@oisann.net>
2014-08-02 20:31:41 +0200

    Test

--- root/parts/pages/nyheter.php
+++ root/parts/pages/nyheter.php
@@ -4,19 +4,22 @@
 	$mysql_connection = mysql_connect($mysql_host,$mysql_user,$mysql_password);
 	if (!$mysql_connection) die('{ "error":"mysql connection failed" }');
 	if (!mysql_select_db($mysql_database, $mysql_connection)) die('{ "error":"mysql database not found" }');
-	$mysql_selector = "SELECT * FROM nyheter WHERE (id = " . $id . ") ORDER BY endret DESC LIMIT 0, 15;";
+	$mysql_selector = "SELECT * FROM nyheter ORDER BY endret DESC LIMIT 0, 15;";
 
 	$mysql_query = mysql_query($mysql_selector, $mysql_connection);
 	if(!$mysql_query) die('{ "error":"' . mysql_error() . '" }');
-	$error = true;
 	while ($row = mysql_fetch_array($mysql_query)) {
-		$error = false;
-		echo "			<h1>" . $row['overskrift'] . "</h1>\n";
-		echo "			" . htmlentities($row['tekst']) . "\n";
-	}
-	if($error) {
-		echo "			<h1>Error 11</h1>\n";
-		echo "			Fant ingen nyhet med id: " . $id . "\n";
+		if(empty($id) {
+			echo '<div class="news"><h1 class="center">Nyheter</h1><table class="stihknews"><tr><td><span class="loading" title="Loading..."></span></td></tr></table></div>';
+		} else {
+			if($row['id'] == $id) {
+				echo "			<h1>" . htmlentities($row['overskrift']) . "</h1>\n";
+				echo "			" . htmlentities($row['tekst']) . "\n";
+			} else {
+				echo "			<h1>Error 11</h1>\n";
+				echo "			Fant ingen nyhet med id: " . $id . "\n";
+			}
+		}
 	}
 	mysql_close($mysql_connection);
 ?>
