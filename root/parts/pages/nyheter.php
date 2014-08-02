9438928c6c5d67b63ecb983377f5fda8cc8c41d6
Jonas <jonas@oisann.net>
2014-08-02 20:48:27 +0200

    Nyheter

--- root/parts/pages/nyheter.php
+++ root/parts/pages/nyheter.php
@@ -8,18 +8,31 @@
 
 	$mysql_query = mysql_query($mysql_selector, $mysql_connection);
 	if(!$mysql_query) die('{ "error":"' . mysql_error() . '" }');
+	$error = true;
 	while ($row = mysql_fetch_array($mysql_query)) {
 		if($id == 'error') {
 			echo '<div class="news"><h1 class="center">Nyheter</h1><table class="stihknews"><tr><td><span class="loading" title="Loading..."></span></td></tr></table></div>';
+			$error = false;
+			return;
 		} else {
 			if($row['id'] == $id) {
 				echo "			<h1>" . htmlentities($row['overskrift']) . "</h1>\n";
+				$facebook_like = '<iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fstihk.no%2Fnyheter%2F' . $id . '&amp;width&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;share=true&amp;height=21&amp;appId=172164879660704" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:21px;" allowTransparency="true"></iframe>';
+				if($row['laget'] != $row['endret']) {
+					echo "			<span class=\"byline\">Av: " . htmlentities($row['av']) . " - Laget: " . date("d.m.Y H:i:s", $row['laget']) . " - Endret: " . date("d.m.Y H:i:s", $row['endret']) . "</span>\n";
+				} else {
+					echo "			<div class=\"byline\">Av: " . htmlentities($row['av']) . " - Laget: " . date("d.m.Y H:i:s", $row['laget']) . "</span>\n";
+				}
 				echo "			" . htmlentities($row['tekst']) . "\n";
-			} else {
-				echo "			<h1>Error 11</h1>\n";
-				echo "			Fant ingen nyhet med id: " . $id . "\n";
+				echo "			<div class=\"social\">Lenke: <input type=\"text\" value=\"http://STI.HK/n/" . $id . "\"  disabled> " . $facebook_like . "</div>";
+				$error = false;
+				return;
 			}
 		}
+	}
+	if(!$error) {
+		echo "			<h1>Error 11</h1>\n";
+		echo "			Fant ingen nyhet med id: " . $id . "\n";
 	}
 	mysql_close($mysql_connection);
 ?>
