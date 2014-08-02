542f1caa9f230cdaec6e69a3fd34593b0b4cc801
Jonas <jonas@oisann.net>
2014-08-02 18:53:04 +0200

    juup

--- root/assets/json/istider.php
+++ root/assets/json/istider.php
@@ -4,11 +4,19 @@
 	header('Expires: ' . date("D, d M Y G:i:s T", strtotime("+1 week")) );
 	header('Content-type: application/json, charset=utf-8');
 	
-	die("ID: " . $_GET['id']);
-
+	$id = $_GET['id'];
+	$sesong = $_GET['season'];
+	
+	if(empty($id) || empty($sesong)) die('{ "error" : "Missing arguments" }');
+	
+	$istider = glob('../../istider/' . $sesong . 'uke_*' . $id . '.htm');
+	foreach($istider as $istid) {
+		echo $istid . '<br>';
+	}
+	die();
 	$articles = array();
 	while ($row = mysql_fetch_array($mysql_query)) {
-
+		
 		$articles[$array_count] = array('id' => intval($row['id']), 'enabled' => $aktiv, 'code' => htmlentities($row['kode']), 'short' => htmlentities($row['kortnavn']), 'name' => htmlentities($row['navn']), 'listname' => htmlentities($row['listenavn']));
 	}
 	
