1699a75a1ee059ca5e8a0f3b8c48a44490404020
Jonas <jonas@oisann.net>
2014-08-02 18:28:14 +0200

    debuug

--- root/assets/js/istider.js
+++ root/assets/js/istider.js
@@ -7,11 +7,14 @@ $.ajax({
 
 
 function updateIshaller(json) {
-	console.log(json);
-	$('select#ishall').html("<option data-id=\"NaN\" disabled>Velg en ishall</option>");
+	$('select#ishall').html("<option data-id=\"NaN\" selected disabled> - Velg en ishall - </option>");
 	for (property in json) {
 		var disabled = "";
 		if(!json[property].enabled) disabled = " disabled";
 		$('select#ishall').append("<option data-id=\"" + json[property].id + "\" " + disabled + ">" + json[property].name + "</option>");
 	}
 }
+
+$('select#ishall').change(function() {
+	console.log(this.selected.text);
+});
