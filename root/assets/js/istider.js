b4bd3117f0f76589fa5c2f0b14b38bc77b222c65
Jonas <jonas@oisann.net>
2014-08-02 18:14:12 +0200

    hmm

--- root/assets/js/istider.js
+++ root/assets/js/istider.js
@@ -1,12 +1,12 @@
-function getIshaller() {
-	$.ajax({
-		type: "GET",
-		url: "http://stihk.no/assets/json/ishaller.json",
-		dataType: "json",
-		success: updateIshaller
-	});
-}
+$.ajax({
+	type: "GET",
+	url: "http://stihk.no/assets/json/ishaller.json",
+	dataType: "json",
+	success: updateIshaller
+});
+
 
 function updateIshaller(json) {
 	console.log(json);
+	$('select#ishall').html("<option data-id=\"NaN\" disabled>Velg en ishall</option>");
 }
--- root/parts/pages/istider.php
+++ root/parts/pages/istider.php
@@ -2,14 +2,13 @@
 			<h1>Istider <?php echo $funksjoner->sesong(); ?></h1>
 			<div class="settings">
 				<select id="ishall">
-					<option selected disabled>Laster Ishaller</option>
+					<option selected disabled>Laster inn ishaller...</option>
 				</select>
 				<select id="uke">
 					<option>Uke 1 - 01.01.14 - 07.01.14</option>
 					<option selected>Uke 2* - 08.01.14 - 14.01.14</option>
 					<option>Uke 3 - 15.01.14 - 22.01.14</option>
 				</select>
-				<input type="button" id="oppdater" value="Oppdater" />
 			</div>
 			<iframe src="./2014_2015/uke_23_leangen2.htm" style="border: 0px;width: 100%;min-height: 100%;"></iframe>
 			<script src="<?php echo $funksjoner->fix_linking(); ?>assets/js/istider.js"></script>
