8dc4995de88da1d6f199ad3cf7a01b1c42a83485
Jonas <jonas@oisann.net>
2014-08-04 16:25:16 +0200

    wooow

--- root/parts/pages/istider.php
+++ root/parts/pages/istider.php
@@ -13,11 +13,10 @@
 			<script src="<?php echo $funksjoner->fix_linking(); ?>assets/js/istider.js"></script>
 			<div class="istider-liste">
 				<?php
-					$istider = glob('../../istider/' . $sesong . '/*.htm');
+					$istider = glob('../../istider/' . $funksjoner->sesong(); . '/*.htm');
 					foreach($istider as $uke) {
 						echo "<a href=\"$uke\">" . $uke . "</a><br>";
 					}
-					echo '../../istider/' . $sesong . '/*.htm';
 				?>
 			</div>
 		</div>
