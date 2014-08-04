c561984fb0f07b283cf50fc9c8232a0116144a37
Jonas <jonas@oisann.net>
2014-08-04 15:05:31 +0200

    kontor fix

--- root/index.php
+++ root/index.php
@@ -14,6 +14,7 @@
 	if(!file_exists("./parts/pages/" . $page . ".php")) $page = "404";
 	$page_include = "./parts/pages/" . $page . ".php";
 
+	if($page == "kontor") header('Location: /kontor/index.html');
 	if($page == "404") $title = "Siden kan ikke vises - Error 404";
 	
 	include('./parts/header.php');
