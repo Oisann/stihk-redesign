a68c77a2e3ddd3c3166f6b9dc733283ef6643b4b
Jonas <jonas@oisann.net>
2014-08-04 16:22:28 +0200

    derp

--- root/parts/pages/istider.php
+++ root/parts/pages/istider.php
@@ -13,7 +13,7 @@
 			<script src="<?php echo $funksjoner->fix_linking(); ?>assets/js/istider.js"></script>
 			<div class="istider-liste">
 				<?php
-					$istider = glob('../../istider/' . $sesong . '/*.htm');
+					$istider = glob('../../../istider/' . $sesong . '/*.htm');
 					foreach($istider as $uke) {
 						echo "<a href=\"$uke\">" . $uke . "</a><br>";
 					}
