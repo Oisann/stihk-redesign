27de79dfaf79f54b44f01e7555b5dd7369951147
Jonas <jonas@oisann.net>
2014-08-13 21:08:29 +0200

    Nope

--- root/assets/js/script.js
+++ root/assets/js/script.js
@@ -4,11 +4,6 @@ var trondheim_url = "https://oisann.net/yr/Norge/S%C3%B8r-Tr%C3%B8ndelag/Trondhe
 	symbol_url_start = "http://symbol.yr.no/grafikk/sym/b38/", //Start of weathersymbol. Ends with .png
 	news_url = "http://stihk.no/demo/assets/json/news.json",
 	socket = io.connect('http://www.oisann.net:3000');
-	socket.set('transports', ['websocket'
-   , 'flashsocket'
-   , 'htmlfile'
-   , 'xhr-polling'
-   , 'jsonp-polling']);
 $(document).ready(function() { //no need for this, since i load it at the bottom of the page. EH
 	var path = $(location).attr('href');
 	try {
