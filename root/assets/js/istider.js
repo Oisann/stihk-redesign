677700f89e4efd6c2c56c7d93175f468d1534a07
Jonas Refseth <jonas@oisann.net>
2015-01-03 21:00:42 +0100

    Shareable istider
    Test, should work.

--- root/assets/js/istider.js
+++ root/assets/js/istider.js
@@ -1,4 +1,26 @@
-﻿var selected_ishall = "",
+﻿$.urlParam = function(name){
+    var results = new RegExp('[\?&#]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results===null){
+	return null;
+    } else {
+	return results[1] || 0;
+    }
+}
+
+$.setUrlParam = function(param, data) {
+	var before = new RegExp('[\?&#]' + param + '=([^&#]*)').exec(window.location.hash);
+	if(before===null) {
+		return null;
+	} else {
+		window.location.hash = (window.location.hash).replace(param + '=' + before[1], param + '=' + data);
+		
+		return true;
+	}
+}
+
+if(window.location.hash=="") window.location.hash = '#h=null&w=null'; //add a shareable settings
+
+var selected_ishall = "",
 	season = $("span#season").text().replace("/", "_");
 
 /*$.ajax({
@@ -21,10 +43,16 @@ Date.prototype.getWeekNumber = function(){
 
 function updateIshaller(json) {
 	$('select#ishall').html("<option data-id=\"NaN\" selected disabled> - Velg en ishall - </option>");
+	var index = 0;
 	for (property in json) {
 		var disabled = "";
 		if(!json[property].enabled) disabled = " disabled";
 		$('select#ishall').append("<option data-id=\"" + json[property].listname + "\" " + disabled + ">" + json[property].name + "</option>");
+		if($.urlParam('h') === json[property].listname) {
+			$('select#ishall')[0].selectedIndex = index;
+			$('select#ishall').trigger("change");
+		}
+		index++;
 	}
 }
 
@@ -35,6 +63,7 @@ function updateUker(json) {
 		var only_week = json[Object.keys(json)[0]].week;
 		if(only_week !== undefined) $('iframe#istid').attr("src", "./" + season + "/uke_" + only_week + "_" + selected_ishall + ".htm");
 	}
+	var index = 0;
 	for (property in json) {
 		if(json[property].error) {
 			var err = json[property].error == 'no files found' ? 'Istidene for denne hallen er ikke ute enda.' : json[property].error;
@@ -49,6 +78,11 @@ function updateUker(json) {
 			$('iframe#istid').attr("src", "./" + season + "/uke_" + (uke<=9?"0"+uke:uke) + "_" + selected_ishall + ".htm");
 		}
 		$('select#uke').append("<option data-week=\"" + json[property].week + "\" " + select + ">Uke " + json[property].week + star + " - " + json[property].first + "-" + json[property].last + "</option>");
+		if($.urlParam('w') === json[property].week) {
+			$('select#uke')[0].selectedIndex = index;
+			$('select#uke').trigger("change");
+		}
+		index++;
 	}
 }
 
@@ -56,6 +90,7 @@ $('select#uke').change(function() {
 	$("select#uke option:selected" ).each(function() {
 		var selected_week = $(this).attr("data-week");
 		$('iframe#istid').attr("src", "./" + season + "/uke_" + selected_week + "_" + selected_ishall + ".htm");
+		$.setUrlParam('q', selected_week);
 	});
 });
 
@@ -71,6 +106,7 @@ $('select#ishall').change(function() {
 			success: updateUker
 		});*/
 		$.get("http://stihk.no/assets/json/istider.json?id=" + selected_ishall + "&season=" + season, function( data ) {
+			$.setUrlParam('h', selected_ishall);
 			updateUker(data);
 		});
     });
